45e82916c4f9b2e587660e57b2bf957326f6f5d7
DeadShotPraveen <shakthi.praveen1010@gmail.com>
2020-08-27 18:56:06 +0530

    deploy

--- tweetme/settings.py
+++ tweetme/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'tweetme.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,"build")],
+        'DIRS': [os.path.join(BASE_DIR,"templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
